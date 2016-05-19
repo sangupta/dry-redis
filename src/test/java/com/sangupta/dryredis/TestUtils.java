8c0e88a207a43fab9a33b7fcc1f00e81ebff43e0
Sandeep Gupta <sandy.pec@gmail.com>
2016-05-19 12:14:41 +0530

    Adding copyright header

--- src/test/java/com/sangupta/dryredis/TestUtils.java
+++ src/test/java/com/sangupta/dryredis/TestUtils.java
@@ -1,3 +1,24 @@
+/**
+ *
+ * dry-redis: In-memory pure java implementation to Redis
+ * Copyright (c) 2016, Sandeep Gupta
+ * 
+ * http://sangupta.com/projects/dry-redis
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 		http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
 package com.sangupta.dryredis;
 
 import java.util.ArrayList;
